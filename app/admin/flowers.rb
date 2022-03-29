5c1d1ace69b9bf1465991419905d81ae2629bf71
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-03-29 15:13:42 -0500

    Add Flower and Category resource to active admin

---
+++ app/admin/flowers.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Flower do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :description, :image, :price, :sale_praice
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :description, :image, :price, :sale_praice]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
