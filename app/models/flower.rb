3852a0ac0f3cb9b453dead31abb9ded00be34e4d
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-03-29 15:43:31 -0500

    Add validations and associations

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,2 +1,5 @@
 class Flower < ApplicationRecord
+  validates :name, :price, presence: true
+  has_many :cart_items
+  belongs_to :category
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ApplicationRecord
+  validates :subtotal, :pst, :gst, :hst, :total, :status, presence: true
+  belongs_to :customer
+  has_many :cart_items
 end
