6a046cfc1ee6969f4643c4857abe62fa7ea86360
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-03-29 19:05:27 -0500

    Populate flower page and about and contact visible

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,7 +1,9 @@
 class FlowersController < ApplicationController
   def index
+    @flowers = Flower.all
   end
 
   def show
+    @flower = Flower.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'contact/show'
+  get 'about/show'
   get 'flowers/index'
   get 'flowers/show'
+
+  root to: "home#index"
+
+  get "/about", to: "about#show"
+  get "/contact", to: "contact#show"
+
+  resources :flowers
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
