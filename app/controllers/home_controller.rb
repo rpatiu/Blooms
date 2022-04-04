1278e883eb5d2873362c857674a1ec93c3c4f5f9
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-04 12:33:08 -0500

    Add functional filters on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,13 @@ class HomeController < ApplicationController
   def index
     @flowers = Flower.page(params[:page]);
   end
+
+  def sale
+    @flowers = Flower.where("sale_price IS NOT NULL").page(params[:page])
+  end
+
+  def new
+    three_days_ago = DateTime.now - 3.days
+    @flowers = Flower.where("created_at > ?", three_days_ago).page(params[:page])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,18 @@ Rails.application.routes.draw do
   get "/about", to: "about#show"
   get "/contact", to: "contact#show"
 
+  get '/index', to:'home#index'
+  get '/sale', to:'home#sale'
+  get '/new' , to:'home#new'
+
   resources :flowers, only: [:index, :show] do
     collection do
       get "search"
     end
   end
 
+
+  resources :home
   resources :category
 
   devise_for :admin_users, ActiveAdmin::Devise.config
