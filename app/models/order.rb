06c9915a3d704b7e8dc11c2c59ae5f6bcc4c7e4e
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-04 16:02:08 -0500

    Adds numericality validation for all models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   validates :subtotal, :pst, :gst, :hst, :total, :status, presence: true
+  validates :pst, :gst, :hst, :total, numericality: { greater_than_or_equal_to: 0}
+
   belongs_to :customer
   has_many :cart_items
 end
--- app/models/province.rb
+++ app/models/province.rb
@@ -1,4 +1,5 @@
 class Province < ApplicationRecord
   validates :name, :pst, :hst, presence: true
+  validates :pst, :gst, :hst, numericality: { greater_than_or_equal_to: 0}
   has_many :customers
 end
