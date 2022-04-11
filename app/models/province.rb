ea72f907b0e843d245f1a845207e844f84137324
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-11 10:28:31 -0500

    Update validations for category and province

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,5 +1,6 @@
 class Province < ApplicationRecord
   validates :name, :pst, :hst, presence: true
   validates :pst, :gst, :hst, numericality: { greater_than_or_equal_to: 0}
+
   has_many :customers
 end
