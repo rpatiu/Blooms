35dafb31e2a344d283519e098588cd7a2c04dc8c
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-19 15:10:09 -0500

    Shopping cart saved session

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -6,13 +6,20 @@ class CartController < ApplicationController
 
     unless session[:shopping_cart].include? (id)
       session[:shopping_cart] << id # push id onto the end of the cart array
+      product = Flower.find(id)
+      flash[:notice] = "+ #{product.name} added to cart."
+      redirect_to root_path
     end
 
-    redirect_to root_path
+
   end
 
   def destroy
-
+    id = params[:id].to_i
+    session[:shopping_cart].delete(id)
+    product = Flower.find(id)
+    flash[:notice] = "- #{product.name} removed from cart."
+    redirect_to root_path
   end
 
 end
