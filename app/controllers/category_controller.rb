deb1a31af1b0ba1349c2f9d09a9a2a930a84e459
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-20 00:17:34 -0500

    Flowers, About and Contact styled

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,6 +2,7 @@ class CategoryController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @flowers = Flower.where("category_id = ?", @category.id).page(params[:page])
 
     add_breadcrumb "#{@category.name.capitalize}", category_path
   end
