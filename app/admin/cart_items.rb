bc38f9a9d7a079502e2d4cd54378db17b76ef10b
Rasmin Patiu <rpatiu38@academic.rrc.com>
2022-04-19 20:22:45 -0500

    Finish up checkout

---
+++ app/admin/cart_items.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register CartItem do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :price, :quantity, :order_id, :flower_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:price, :quantity, :order_id, :flower_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
